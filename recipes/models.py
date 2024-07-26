1751ae0378398b4bda8b56af0f68108b92d3cb07
alexandraspadafino <25alexspad@gmail.com>
2024-07-25 22:48:01 -0700

    Created base models

--- recipes/models.py
+++ recipes/models.py
@@ -1,19 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class DietaryRestriction(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Unit(models.Model):
+    symbol = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.symbol
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=250)
+    units = models.ManyToManyField(Unit, through='IngredientQuantity')
+
+    def __str__(self):
+        return self.name
+    
+class IngredientQuantity(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    quantity = models.CharField(max_length=25)  # Or DecimalField depending on your needs
+
+    def __str__(self):
+        return f"{self.quantity} {self.unit.symbol} of {self.ingredient.name}"
+
+    
 # primary key
 class Recipe(models.Model):
     title = models.CharField(max_length=250)
-    ingredients = models.JSONField()
+    ingredients = models.ManyToManyField(IngredientQuantity, blank=True)
+    Dietary_Restrictions = models.ManyToManyField(DietaryRestriction, blank=True)
 
     def __str__(self):
         return self.title
-
-#foreign key that belongs to a primary key
-class Allergen(models.Model):
-    allergen = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.allergen
     
 
